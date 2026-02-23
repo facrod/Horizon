27e991e419f621a16e774c76d555ce568254398b
facrod <facundorodriguez494@gmail.com>
2026-02-23 20:59:42 -0300

    Se añadieron estilos y se cambiaron etiquetas para hacerlo semanticamente correcto, se agregaron mediaqueries para hacerlo responsive

--- index.js
+++ index.js
@@ -3,17 +3,27 @@
 let botonMostrarDias = document.getElementById('mostrarDias');
 const listaDias = document.getElementById('listaDias')
 
-botonMostrarDias.addEventListener("click", ()=>{
+botonMostrarDias.addEventListener("click", (e)=>{
+    e.stopPropagation()
     listaDias.classList.toggle("abierto");
 })
 
 let btnDesplegar = document.getElementById("btnDesplegar");
 let agregarTarea = document.getElementById("agregarTarea");
 
-btnDesplegar.addEventListener("click", ()=>{
+btnDesplegar.addEventListener("click", (e)=>{
+    e.stopPropagation()
     agregarTarea.classList.toggle("activo")
 })
+document.addEventListener("click", (e) => {
+    if (!botonMostrarDias.contains(e.target) && !listaDias.contains(e.target)) {
+        listaDias.classList.remove("abierto");
+    }
 
+    if (!btnDesplegar.contains(e.target) && !agregarTarea.contains(e.target)) {
+        agregarTarea.classList.remove("activo");
+    }
+});
 //fin botones header
 
 // Logica para agregar una tarea nueva al local storage
@@ -22,6 +32,7 @@ let diaTarea = document.getElementById("diaTarea");
 let horaTarea = document.getElementById("horaTarea");
 let descripcionTarea = document.getElementById("descripcionTarea");
 let btnAgregarTarea = document.getElementById("btnAgregarTarea");
+const formAgregar = document.getElementById("agregarTarea");
 
 let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 const tablero = document.getElementById("tablero");
@@ -48,25 +59,78 @@ function pintarTarea(tarea) {
         </article>
     `
 }
+function reenderizarTarea(tarjeta) {
+    let nuevoContenido = `
+        <img src="img/cerrar.png" class="cerrar" id="close" alt="cerrar">
+        
+        <section id="seccionDescripcionInfo">
+            <article class="info-descripcion">
+                <h3>${tarjeta.tarea}</h3>
+                <p>${tarjeta.descripcion}</p>    
+            </article>
+            <article class="dia-hora-descripcion">
+                <p>${tarjeta.dia}</p>
+                <p>${tarjeta.hora}</p>
+                <p class="estadoMsj">Estado: ${tarjeta.estado ? "Completa" : "incompleta"}</p>
+            </article>
+        </section>
 
+        <form id="inputsModificar" >
+            <label for="tareaModificada">Tarea</label>
+            <input type="text" id="tareaModificada" class="modificarT">
 
+            <label for="descripcionInput">Descripción</label>
+            <textarea id="descripcionInput" class="modificarT" cols="30" rows="10"></textarea>
+
+            <label for="diaModificado">Fecha</label>
+            <input type="date" id="diaModificado" class="modificarT">
+
+            <label for="horaModificada">Hora</label>
+            <input type="time" id="horaModificada" class="modificarT">
+
+            <input type="submit" id="btnModificar" class="modificarT" value="modificar">
+        </form>
+
+        <article class="acciones-descripcion">
+            <button class="accion-editar" data-id="${tarjeta.id}">
+                <p>editar</p>    
+                <img src="img/editar.png" alt="editar">
+            </button>
+            <button class="accion-eliminar" data-id="${tarjeta.id}">
+                <p>eliminar</p>    
+                <img src="img/eliminar.png" alt="elimininar">
+            </button>
+            <label class="custom-checkbox" data-id="${tarjeta.id}">
+                <input type="checkbox" ${tarjeta.estado ? "checked" : ""}>
+                <span class="checkmark"> 
+                    <img src="img/checkBlanco.png" alt="completado"> 
+                </span>
+                Estado
+            </label>
+        </article>
+        `;
+        return nuevoContenido;
+}
 let inputs = [nombreTarea, diaTarea, horaTarea, descripcionTarea];
 
-btnAgregarTarea.addEventListener("click", (e) => {
+
+formAgregar.addEventListener("submit", (e) => {
     e.preventDefault();
+
     let formularioValido = true;
+
     inputs.forEach(input => {
         if (!input.value.trim()) {
-            input.style.border = "1.5px solid #ff4d4d"; 
+            input.style.border = "1.5px solid #ff4d4d";
             formularioValido = false;
         } else {
-            input.style.border = "none"; 
+            input.style.border = "none";
         }
     });
 
     if (!formularioValido) {
-        alert("Faltan rellenar campos")
-        return; 
+        alert("Faltan rellenar campos");
+        return;
     }
 
     let tarea = {
@@ -83,13 +147,8 @@ btnAgregarTarea.addEventListener("click", (e) => {
 
     pintarTarea(tarea);
 
-    inputs.forEach(input => {
-        input.value = "";
-        input.style.border = "none"; 
-    });
-    
+    formAgregar.reset(); 
     agregarTarea.classList.remove("activo");
-    
 });
 
 
@@ -112,51 +171,6 @@ tablero.addEventListener("click", (e) => {
     const tareaEncontrada = tareas.find(t => t.id === tareaId);
     if (!tareaEncontrada) return;
 
-    let diaParseado = tareaEncontrada.dia.split("T");
-
-    const nuevoContenido = `
-        <img src="img/cerrar.png" class="cerrar" id="close" alt="cerrar">
-        
-        <section id="seccionDescripcionInfo">
-            <article class="info-descripcion">
-                <h3>${tareaEncontrada.tarea}</h3>
-                <p>${tareaEncontrada.descripcion}</p>    
-            </article>
-            <article class="dia-hora-descripcion">
-                <p>${diaParseado[0]}</p>
-                <p>${tareaEncontrada.hora}</p>
-                <p class="estadoMsj">Estado: ${tareaEncontrada.estado ? "Completa" : "incompleta"}</p>
-            </article>
-        </section>
-        <section id="inputsModificar" hidden>
-            <input type="text" id="tareaModificada" class="modificarT" >
-            <input type="text" id="descripcionInput" class="modificarT" >
-            <input type="date" id="diaModificado" class="modificarT" >
-            <input type="time" id="horaModificada" class="modificarT" >
-            <input type="button" value="modificar" class="modificarT" id="btnModificar">
-        </section>
-        
-
-        <article class="acciones-descripcion">
-            <button class="accion-editar" data-id="${tareaEncontrada.id}">
-                <p>editar</p>    
-                <img src="img/editar.png" alt="editar">
-            </button>
-            <button class="accion-eliminar" data-id="${tareaEncontrada.id}">
-                <p>eliminar</p>    
-                <img src="img/eliminar.png" alt="elimininar">
-            </button>
-            <label class="custom-checkbox" data-id="${tareaEncontrada.id}">
-                <input type="checkbox" ${tareaEncontrada.estado ? "checked" : ""}>
-                <span class="checkmark"> 
-                    <img src="img/checkBlanco.png" alt="completado"> 
-                </span>
-                Estado
-            </label>
-        </article>
-        `;
-
-
     //REPASAR FUERTEMENTE ESTO
     //si esta visible prendemos(fade-out)
     if (seccionDescripcion.classList.contains("activo")) {
@@ -164,7 +178,7 @@ tablero.addEventListener("click", (e) => {
         seccionDescripcion.classList.remove("activo");
 
         const handler = () => {
-            seccionDescripcion.innerHTML = nuevoContenido;
+            seccionDescripcion.innerHTML = reenderizarTarea(tareaEncontrada);
             seccionDescripcion.classList.add("activo");
             setTimeout(() => {
                     seccionDescripcion.scrollIntoView({ 
@@ -179,7 +193,7 @@ tablero.addEventListener("click", (e) => {
 
     } else {
         // Si estaba apagado, prendemos
-        seccionDescripcion.innerHTML = nuevoContenido;
+        seccionDescripcion.innerHTML = reenderizarTarea(tareaEncontrada);
         seccionDescripcion.classList.add("activo");
         setTimeout(() => {
                 seccionDescripcion.scrollIntoView({ 
@@ -187,10 +201,9 @@ tablero.addEventListener("click", (e) => {
                     block: 'center' 
                 });
             }, 200);
-
     }
-
 });
+
 
 seccionDescripcion.addEventListener("click", (e) => {
     if (e.target.closest(".cerrar")) {
@@ -198,84 +211,28 @@ seccionDescripcion.addEventListener("click", (e) => {
         return;
     }
 
-            // EDITAR
+            // EDITAR (ABRE LA SECCIÓN PARA MODIFICAR)
 
     if (e.target.closest(".accion-editar")) {
+        let inputsModificar = document.getElementById("inputsModificar");
+        let inputsDescripcion = document.getElementById("seccionDescripcionInfo");
         let idTarea = e.target.closest(".accion-editar").dataset.id;
         let tar = tareas.find(t => t.id == Number(idTarea));
-        let elemento = document.getElementById("inputsModificar")
-        elemento.hidden = false
-        let inputsDescripcion = document.getElementById("seccionDescripcionInfo")
-        inputsDescripcion.hidden = true
-        document.getElementById("tareaModificada").value = tar.tarea;
-        document.getElementById("descripcionInput").value = tar.descripcion;
-        document.getElementById("diaModificado").value = tar.dia;
-        document.getElementById("horaModificada").value = tar.hora;
+        if (!inputsModificar.classList.contains("visible")) {
+            inputsModificar.classList.add("visible");
+            inputsDescripcion.style.display = "none";
 
-        //Empezando lógica para guardar los valores editados en la misma tarea
-
-        let btnModificar = document.getElementById("btnModificar")
-        btnModificar.onclick = () => {
-            let tareaModificada = document.getElementById("tareaModificada").value
-            let descripcionModificada = document.getElementById("descripcionInput").value
-            let diaModificado = document.getElementById("diaModificado").value
-            let horaModifcada = document.getElementById("horaModificada").value
-
-            tar.tarea = tareaModificada;
-            tar.descripcion = descripcionModificada;
-            tar.dia = diaModificado;
-            tar.hora = horaModifcada
-            localStorage.setItem("tareas", JSON.stringify(tareas));
-
-            const nuevoContenido = `
-            <img src="img/cerrar.png" class="cerrar" id="close" alt="cerrar">
+            document.getElementById("tareaModificada").value = tar.tarea;
+            document.getElementById("descripcionInput").value = tar.descripcion;
+            document.getElementById("diaModificado").value = tar.dia;
+            document.getElementById("horaModificada").value = tar.hora;
             
-            <section id="seccionDescripcionInfo">
-                <article class="info-descripcion">
-                    <h3>${tar.tarea}</h3>
-                    <p>${tar.descripcion}</p>    
-                </article>
-                <article class="dia-hora-descripcion">
-                    <p>${tar.dia}</p>
-                    <p>${tar.hora}</p>
-                    <p class="estadoMsj">Estado: ${tar.estado ? "Completa" : "incompleta"}</p>
-                </article>
-            </section>
-            <section id="inputsModificar" hidden>
-                <input type="text" id="tareaModificada" class="modificarT" >
-                <input type="text" id="descripcionInput" class="modificarT" >
-                <input type="date" id="diaModificado" class="modificarT" >
-                <input type="time" id="horaModificada" class="modificarT" >
-                <input type="button" value="modificar" class="modificarT" id="btnModificar">
-            </section>
-            
-
-            <article class="acciones-descripcion">
-                <button class="accion-editar" data-id="${tar.id}">
-                    <p>editar</p>    
-                    <img src="img/editar.png" alt="editar">
-                </button>
-                <button class="accion-eliminar" data-id="${tar.id}">
-                    <p>eliminar</p>    
-                    <img src="img/eliminar.png" alt="elimininar">
-                </button>
-                <label class="custom-checkbox" data-id="${tar.id}">
-                    <input type="checkbox" ${tar.estado ? "checked" : ""}>
-                    <span class="checkmark"> 
-                        <img src="img/checkBlanco.png" alt="completado"> 
-                    </span>
-                    Estado
-                </label>
-            </article>
-            `;
-            seccionDescripcion.innerHTML = "";
-            seccionDescripcion.innerHTML = nuevoContenido;
-            elemento.hidden = true
-            inputsDescripcion.hidden = false
-            let tarjetaTablero = document.getElementById(`${tar.id}`)
-            tarjetaTablero.remove()
-            pintarTarea(tar)
-        }
+            // Guardamos el ID en el form para saber cuál TAREA estamos editando
+            inputsModificar.dataset.id = idTarea; 
+        } else {
+            inputsModificar.classList.remove("visible");
+            inputsDescripcion.style.display = "block";
+        }   
     }
 
             //ELIMINAR
@@ -292,7 +249,27 @@ seccionDescripcion.addEventListener("click", (e) => {
         seccionDescripcion.classList.remove("activo");
     }
 });
+seccionDescripcion.addEventListener("submit", (e) => {
+    if (e.target.id === "inputsModificar") {
+        e.preventDefault(); 
 
+        let idTarea = e.target.dataset.id;
+        let tar = tareas.find(t => t.id == Number(idTarea));
+
+        tar.tarea = document.getElementById("tareaModificada").value;
+        tar.descripcion = document.getElementById("descripcionInput").value;
+        tar.dia = document.getElementById("diaModificado").value;
+        tar.hora = document.getElementById("horaModificada").value;
+
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+
+        let tarjetaTablero = document.getElementById(`${tar.id}`);
+        if (tarjetaTablero) tarjetaTablero.remove();
+        pintarTarea(tar);
+
+        seccionDescripcion.innerHTML = reenderizarTarea(tar);
+    }
+});
 seccionDescripcion.addEventListener("change", (e) => {
     if (!e.target.matches('input[type="checkbox"]')) return;
 
@@ -312,15 +289,3 @@ seccionDescripcion.addEventListener("change", (e) => {
 
     console.log(tareaEncontrada);
 });
-/*
-    let tareaClick = document.getElementById(tareas.id)
-    const seccionDescripcion = document.getElementById("seccion-descripcion")
-    tareaClick.addEventListener("click", ()=>{
-        seccionDescripcion.innerHTML = `
-            <h3> ${tareas.tarea}
-
-    `
-
-    let estadoCheckbox = document.getElementById("")
-    })
-    */
